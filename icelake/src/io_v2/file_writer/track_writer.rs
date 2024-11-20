522938ec8652556818bdf7cd7ffc4b0e7ce02a43
Dylan Chen <zilin@singularity-data.com>
2024-11-20 13:30:13 +0800

    fix

--- icelake/src/io_v2/file_writer/track_writer.rs
+++ icelake/src/io_v2/file_writer/track_writer.rs
@@ -29,9 +29,6 @@ impl tokio::io::AsyncWrite for TrackWriter {
     ) -> std::task::Poll<std::result::Result<usize, std::io::Error>> {
         match Pin::new(&mut self.writer).poll_write(cx, buf) {
             std::task::Poll::Ready(Ok(n)) => {
-                if n != buf.len() {
-                    println!("previous bug, n: {}, buf.len(): {}", n, buf.len());
-                }
                 self.written_size
                     .fetch_add(n as i64, std::sync::atomic::Ordering::Relaxed);
                 std::task::Poll::Ready(Ok(n))
